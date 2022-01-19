dd745d4b79684d2106793871d144bc3e50430e9a
Shahid khan <khansahab3102@gmail.com>
2022-01-19 15:54:04 +0530

    for counting the total characters in a string

---
+++ src/Strings_question.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+//Take an array of Strings input from the user & find the cumulative (combined) length of all those strings.
+public class Strings_question {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter the no. of inputs you want");
+        int n=s.nextInt(),count=0;
+        String arr[]=new String[n];
+        System.out.println("enter the string");
+        for (int i = 0; i < n ; i++) {
+            arr[i]=s.next();
+            count+=arr[i].length();
+
+        }
+        System.out.println(count);
+    }
+}
