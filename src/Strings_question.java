08fcd32a38ecd269fb74361c082802f79d616c0c
Shahid khan <khansahab3102@gmail.com>
2022-01-19 16:47:17 +0530

    updated and cleaned and can take string full line

--- src/Strings_question.java
+++ src/Strings_question.java
@@ -1,18 +1,22 @@
-import java.util.Scanner;
-
 //Take an array of Strings input from the user & find the cumulative (combined) length of all those strings
-public class Strings_question {
+import java.util.Scanner;
+class Strings_question {
     public static void main(String[] args) {
         Scanner s=new Scanner(System.in);
         System.out.println("enter the no. of inputs you want");
         int n=s.nextInt(),count=0;
         String arr[]=new String[n];
-        System.out.println("enter the string");
+        s.nextLine();
         for (int i = 0; i < n ; i++) {
-            arr[i]=s.next();
+            System.out.println("enter the string");
+            arr[i]=s.nextLine();
+
             count+=arr[i].length();
 
         }
         System.out.println(count);
     }
 }
+
+
+
