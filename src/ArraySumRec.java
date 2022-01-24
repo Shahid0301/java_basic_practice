d7b735f4d6a397747fe714cb2fe2aafb720a6ab0
Shahid khan <khansahab3102@gmail.com>
2022-01-25 00:03:52 +0530

    //to calculate the sum of all elements of array by recursion

---
+++ src/ArraySumRec.java
@@ -0,0 +1,28 @@
+//to calculate the sum of all elements of array by recursion
+import java.util.Scanner;
+
+public class ArraySumRec {
+    public static int Sum(int arr[],int i){
+        int sum=0;
+        if(i>=arr.length){
+            return 0;
+        }
+        sum+=arr[i];
+        i=i;
+        return sum+Sum(arr,i=i+1);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int ans,i=0;
+        //int arr[]={1,2,3,6,6,3,4,5};
+        System.out.println("enter the size of array");
+        int n=sc.nextInt();
+        int arr[]=new int[n];
+        System.out.println("Enter the elements");
+        for (int j = 0; j < n; j++) {
+            arr[j]=sc.nextInt();
+        }
+        ans=Sum(arr,0);
+        System.out.println("the sum of all elements is "+ans);
+    }
+}
