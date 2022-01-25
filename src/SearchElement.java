86633c7925604a3afb430cb574a30336277bb6b8
Shahid khan <khansahab3102@gmail.com>
2022-01-26 01:20:04 +0530

    updated by doing it with simple array

--- src/SearchElement.java
+++ src/SearchElement.java
@@ -4,34 +4,40 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class SearchElement {
-    public static ArrayList find(int arr[], ArrayList tar_arr, int currentIndex, int target){
+    public static int[] find(int arr[], int tar_arr[], int currentIndex, int target,int i){
         if(currentIndex==arr.length){
             return tar_arr;
         }
         if(arr[currentIndex]==target){
-            tar_arr.add(currentIndex);
+            tar_arr[i]=currentIndex;
+            i++;
         }
         currentIndex++;
-        return find(arr,tar_arr,currentIndex,target);
+        return find(arr,tar_arr,currentIndex,target,i);
 
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n,a;
+        int n,a,i=0;
         System.out.println("enter the size of array");
         a=sc.nextInt();
         int arr[]=new int[a];
         System.out.println("enter "+a+" elements in array");
-        for (int i = 0; i <a ; i++) {
-            arr[i]=sc.nextInt();
+        for (int j = 0; j <a ; j++) {
+            arr[j]=sc.nextInt();
         }
         System.out.println("enter the no. you want to find in your array");
         n = sc.nextInt();
-        ArrayList arr1=new ArrayList();
-        ArrayList ans = find(arr,arr1, 0, n);
-        for (int i = 0; i < ans.size(); i++) {
-            System.out.print(ans.get(i)+" ");
+        int arr1[]=new int[arr.length];
+        int ans[] = find(arr,arr1, 0, n,i);
+        for (int j = 0; j < ans.length; j++) {
+            if(j==0){
+                System.out.print(ans[j]+" ");
+            }
+            else if(ans[j]!=0){
+                System.out.print(ans[j]+" ");
+            }
         }
     }
 }
