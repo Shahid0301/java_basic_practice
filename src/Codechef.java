d3762bfe385df63cde5093c43a7219e0e7d93e71
Shahid khan <khansahab3102@gmail.com>
2022-01-29 22:21:52 +0530

    Subset addition

---
+++ src/Codechef.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        // your code goes here
+        Scanner s =new Scanner(System.in);
+        int t=s.nextInt();
+        for (int i = 0; i < t; i++) {
+            int a=s.nextInt();
+            int x=s.nextInt();
+            int y=s.nextInt();
+            int c=1;
+            int[] str1=new int[a];
+            int[] str2=new int[a];
+            for (int j = 0; j < a; j++) {
+                str1[j]=s.nextInt();
+            }
+            for (int j = 0; j < a; j++) {
+                str2[j]=s.nextInt();
+            }
+            for (int j = 0; j < a; j++) {
+                if (((str2[j] -str1[j])!= x) && ((str2[j]-str1[j])!= y)) {
+                    c = 0;
+                    break;
+                }
+             
+            }
+            if(c!=0)
+                System.out.println("Yes");
+            else
+                System.out.println("No");
+
+        }
+    }
+}
