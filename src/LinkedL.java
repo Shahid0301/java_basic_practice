382017e79c102b924225b78b71ecb71e448f5ba4
Shahid khan <khansahab3102@gmail.com>
2022-02-03 13:40:16 +0530

    Linked list practice with addition at first and last ,also displaying that.

---
+++ src/LinkedL.java
@@ -0,0 +1,55 @@
+class LL{
+    Node head;
+    class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    void addFirst(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=newNode;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+    }
+    void addLast(int data){
+        Node newNode=new Node(data);
+        if(head==null){
+            head=newNode;
+            return;
+        }
+        Node temp=head;
+        while (temp.next!=null){
+            temp=temp.next;
+        }
+        temp.next=newNode;
+
+    }
+    void display(){
+        if(head==null){
+            System.out.println("your list is empty");
+        }
+        Node temp=head;
+        while (temp!=null){
+            System.out.print(temp.data+"-->");
+            temp=temp.next;
+        }
+        System.out.println("null");
+    }
+
+}
+public class LinkedL {
+    public static void main(String[] args) {
+        LL list=new LL();
+        list.addLast(2);
+        list.addLast(3);
+        list.addLast(4);
+        list.addFirst(1);
+        list.display();
+    }
+}
